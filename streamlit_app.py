4eb27de3290b49c23dbd1a26fdc069960c1c245c
harika9948 <133083298+harika9948@users.noreply.github.com>
2023-05-10 18:26:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,7 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -35,7 +36,6 @@ except URLError as e:
   streamlit.error()
 
 streamlit.header("The fruit load list contains:")
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 def get_fruit_load_list():
   with my_cnx.Cursor() as my_cur:
     my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
